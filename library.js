ad8b27bf7e107f7a2dd2a43addc2d4b87b9bccda
Safina Lisa <liz.safina@gmail.com>
2021-03-03 16:13:34 -0500

    fixed button, no positioning

--- library.js
+++ library.js
@@ -1,10 +1,11 @@
 let library=[];
+
 //book Constractor
-function Book(title, author, pages, readOrNot){
+function Book(title, author, pages, read){
     this.title = title
     this.author = author
     this.pages = pages                   
-    this.readOrNot = read
+    this.read = read
 }
 //Get the values, create a book, push to the library
 
@@ -17,44 +18,63 @@ var form = document.querySelector('.form').addEventListener('submit', (event)=>
     const title = document.querySelector("#bName").value;
     const author = document.querySelector("#author").value;
     const pages = document.querySelector("#pages").value;
-    let readOrNot = document.querySelector("#read").value;
+    const read = document.querySelector("#isRead").checked;
+    
     // Instantiate a book
-    const newBook = new Book(title,author,pages,readOrNot);
+    const newBook = new Book(title,author,pages,read);
     
     library.push(newBook);
     uploadOnThepage(newBook);
-
     popUpForm.style.display="none";
 
 });
 
-
-
  function uploadOnThepage(newBook){
-     
      const booksList= document.querySelector("#book-list");
+     const btnHolder=document.querySelector(".btnHolder");
      const row = document.createElement('tr');
+     const readBtn = document.createElement("div");
+     readBtn.id="readBtn";
+     btnHolder.appendChild(readBtn);
+     if(newBook.read===false){
+         readBtn.innerHTML ="Not read";
+         readBtn.classList.add('notDone');
+     }else{
+        readBtn.innerHTML ="Read";
+        readBtn.classList.add('doneReading');
+     }
      row.innerHTML =`
      <td>${newBook.title}</td>
      <td>${newBook.author}</td>
      <td>${newBook.pages}</td>
-     <td><label class="switch">
-     <input type="checkbox">
-     <span class="slider round"></span>
-   </label></td>
-     <td><a href="#" id=${library.indexOf(newBook)} class="btn delete">X</a></td>
+     <td><a href="#" class="btn delete">X</a></td>
      `;
-    booksList.appendChild(row);
- 
-
+     
+     
+      booksList.appendChild(row);
+     
  function clearFields(){
      document.querySelector('#bName').value = "";
      document.querySelector('#author').value = "";
      document.querySelector('#pages').value = "";
-     document.querySelector('#read').value = "";
  }
  clearFields();
+ //toggle read button
+ document.querySelector("#readBtn").addEventListener('click',()=>{
+    if(readBtn.innerHTML === "Not read"){
+        readBtn.innerHTML = "Read";
+        readBtn.classList.remove("notDone");
+        readBtn.classList.add("doneReading");
+    }else{
+        readBtn.innerHTML="Not read";
+        readBtn.classList.remove("doneReading");
+        readBtn.classList.add("notDone");
+    }
+
+   })
+
  }
+ 
 
 document.querySelector(".addNewBook").addEventListener('click', function(){
     popUpForm.style.display = 'block';
@@ -79,3 +99,4 @@ function deleteBook(el){
 document.querySelector('#book-list').addEventListener('click',(e)=>{
     deleteBook(e.target);
 })
+
