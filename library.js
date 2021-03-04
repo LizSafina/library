5498f3f3a46d4b67009aaed02aa3d6acc6f8bb34
Safina Lisa <liz.safina@gmail.com>
2021-03-04 02:17:10 -0500

    local storage

--- library.js
+++ library.js
@@ -1,4 +1,5 @@
 let library=[];
+let popUpForm = document.querySelector(".popupForm");
 
 //book Constractor
 function Book(title, author, pages, read){
@@ -7,13 +8,13 @@ function Book(title, author, pages, read){
     this.pages = pages                   
     this.read = read
 }
-//Get the values, create a book, push to the library
 
+//upload to the page
 var form = document.querySelector('.form').addEventListener('submit', (event)=>
 {
     //prevent actual submit
     event.preventDefault();
-   
+    popUpForm.style.display="none";
     //Get form values
     const title = document.querySelector("#bName").value;
     const author = document.querySelector("#author").value;
@@ -22,59 +23,91 @@ var form = document.querySelector('.form').addEventListener('submit', (event)=>
     
     // Instantiate a book
     const newBook = new Book(title,author,pages,read);
-    
     library.push(newBook);
-    uploadOnThepage(newBook);
-    popUpForm.style.display="none";
+    //createAbook(newBook);
+   setData();
+    render();
+    form.reset();
 
 });
 
- function uploadOnThepage(newBook){
-     const booksList= document.querySelector("#book-list");
-     const btnHolder=document.querySelector(".btnHolder");
-     const row = document.createElement('tr');
-     const readBtn = document.createElement("div");
-     readBtn.id="readBtn";
-     btnHolder.appendChild(readBtn);
-     if(newBook.read===false){
-         readBtn.innerHTML ="Not read";
-         readBtn.classList.add('notDone');
-     }else{
-        readBtn.innerHTML ="Read";
-        readBtn.classList.add('doneReading');
-     }
-     row.innerHTML =`
-     <td>${newBook.title}</td>
-     <td>${newBook.author}</td>
-     <td>${newBook.pages}</td>
-     <td><a href="#" class="btn delete">X</a></td>
-     `;
-     
-     
-      booksList.appendChild(row);
-     
- function clearFields(){
-     document.querySelector('#bName').value = "";
-     document.querySelector('#author').value = "";
-     document.querySelector('#pages').value = "";
- }
- clearFields();
- //toggle read button
- document.querySelector("#readBtn").addEventListener('click',()=>{
-    if(readBtn.innerHTML === "Not read"){
-        readBtn.innerHTML = "Read";
-        readBtn.classList.remove("notDone");
-        readBtn.classList.add("doneReading");
-    }else{
-        readBtn.innerHTML="Not read";
-        readBtn.classList.remove("doneReading");
-        readBtn.classList.add("notDone");
+
+//it deletes whats uploaded and then creates all the stored books plus new one from scratch
+function render(){
+    const display = document.getElementById('book-list');
+    const books = document.querySelectorAll('.book');
+    books.forEach(book => display.removeChild(book));
+    for(let i=0; i<library.length; i++){
+        createAbook(library[i]);
     }
+}
 
-   })
 
- }
+
+
+function createAbook(newBook){
+    const booksList= document.querySelector("#book-list");
+    const row = document.createElement('tr');
+    row.classList.add('book');
+    row.innerHTML =`
+    <td>${newBook.title}</td>
+    <td>${newBook.author}</td>
+    <td>${newBook.pages}</td>
+    <td><a href="#" class="btn delete ${library.indexOf(newBook)}">X</a></td>
+    <td><a href="#" class="readBtn" id="${library.indexOf(newBook)}"></a></td> 
+    `;
+    booksList.appendChild(row);
+
+function clearFields(){
+    document.querySelector('#bName').value = "";
+    document.querySelector('#author').value = "";
+    document.querySelector('#pages').value = "";
+}
+clearFields();
+
+const readBtn = document.querySelector(".readBtn");
+function uploadBtn(){  
  
+if(newBook.read===false){
+   readBtn.innerHTML ="Not read";
+   readBtn.classList.add('notDone');
+}else{
+  readBtn.innerHTML ="Read";
+  readBtn.classList.add('doneReading');
+}
+}
+uploadBtn();
+
+
+function deleteBook(el){
+    if(el.classList.contains('delete')){
+       el.parentElement.parentElement.remove();
+       library.splice(library.indexOf(newBook),1);
+       setData();
+    }
+  }
+  //Remove a Book
+  document.querySelector('#book-list').addEventListener('click',(e)=>{
+      deleteBook(e.target);
+      setData();
+  })
+
+readBtn.addEventListener('click',()=>{
+   if(readBtn.innerHTML === "Not read"){
+       readBtn.innerHTML = "Read";
+       readBtn.classList.remove("notDone");
+       readBtn.classList.add("doneReading");
+   }else{
+       readBtn.innerHTML="Not read";
+       readBtn.classList.remove("doneReading");
+       readBtn.classList.add("notDone");
+   }
+
+  })
+
+}
+
+
 
 document.querySelector(".addNewBook").addEventListener('click', function(){
     popUpForm.style.display = 'block';
@@ -85,18 +118,19 @@ document.querySelector(".cancel").addEventListener('click', function(){
 
 
 
-let popUpForm = document.querySelector(".popupForm");
-
-
-function deleteBook(el){
-  if(el.classList.contains('delete')){
-      el.parentElement.parentElement.remove();
-  }
+function setData() {
+    localStorage.setItem(`library`, JSON.stringify(library));
 }
 
+function restore() {
+    if(!localStorage.library) {
+        render();
+    }else {
+        let objects = localStorage.getItem('library')
+        objects = JSON.parse(objects);
+        library = objects;
+        render();
+    }
+}
 
-//Remove a Book
-document.querySelector('#book-list').addEventListener('click',(e)=>{
-    deleteBook(e.target);
-})
-
+restore();
